b6c604c5a29ccef5f9bb078f2f4a8bf07ac5dea1
bastianibanez <bastian.miba@gmail.com>
2025-05-19 11:28:23 -0400

    Programa estable

--- src/app/impl/MyShortestPathProblem.java
+++ src/app/impl/MyShortestPathProblem.java
@@ -16,7 +16,7 @@ public class MyShortestPathProblem implements ShortestPathProblem {
     private List<Integer> path = new ArrayList<>();
     private Set<Integer> visited = new HashSet<>();
 
-    public MyShortestPathProblem(int[][] graph, int start, int end){
+    public MyShortestPathProblem(int[][] graph, int start, int end) {
         this.graph = graph;
         this.start = start;
         this.end = end;
@@ -26,13 +26,13 @@ public class MyShortestPathProblem implements ShortestPathProblem {
     }
 
     @Override
-    public boolean isSolution(){
+    public boolean isSolution() {
         return path.getLast() == end;
     }
 
     @Override
-    public void applyMove(int move){
-        if (path.isEmpty()){
+    public void applyMove(int move) {
+        if (path.isEmpty()) {
             return;
         }
 
@@ -43,8 +43,8 @@ public class MyShortestPathProblem implements ShortestPathProblem {
     }
 
     @Override
-    public void undoMove(int move){
-        if (path.size() <= 1){
+    public void undoMove(int move) {
+        if (path.size() <= 1) {
             return;
         }
 
@@ -56,12 +56,12 @@ public class MyShortestPathProblem implements ShortestPathProblem {
     }
 
     @Override
-    public List<Integer> getPossibleMoves(){
+    public List<Integer> getPossibleMoves() {
         int from = path.getLast();
         List<Integer> moves = new ArrayList<>();
 
-        for (int to = 0; to < graph.length; to++){
-            if (graph[from][to] > 0 && !visited.contains(to)){
+        for (int to = 0; to < graph.length; to++) {
+            if (graph[from][to] > 0 && !visited.contains(to)) {
                 moves.add(to);
             }
         }
@@ -69,17 +69,17 @@ public class MyShortestPathProblem implements ShortestPathProblem {
     }
 
     @Override
-    public List<Integer> getCurrentPath(){
+    public List<Integer> getCurrentPath() {
         return path;
     }
 
     @Override
-    public int getCurrentPathLength(){
+    public int getCurrentPathLength() {
         return path.size();
     }
 
     @Override
-    public MyShortestPathProblem copy(){
+    public MyShortestPathProblem copy() {
         MyShortestPathProblem copy = new MyShortestPathProblem(graph, start, end);
         copy.path = new ArrayList<>(this.path);
         copy.visited = new HashSet<>(this.visited);
