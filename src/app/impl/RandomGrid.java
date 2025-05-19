95c1affee0c1ff2dd0d3af7e7412f8f9ba16b43c
bastianibanez <bastian.miba@gmail.com>
2025-05-19 14:19:35 -0400

    Se creo funcion main y se avanzó en ambos enfoques
    Falta hacer que la solución de SequentialShortestPath funcione bien
    Falta hacer que la función main entrege el resultado correcto

--- src/app/impl/RandomGrid.java
+++ src/app/impl/RandomGrid.java
@@ -7,21 +7,26 @@ public class RandomGrid {
     private int[][] grid;
     private int dimension;
 
-    public RandomGrid(int dimension){
+    public RandomGrid(int dimension) {
+        this.dimension = dimension;
         this.grid = new int[dimension][dimension];
-        for (int i = 0; i < dimension; i++){
-            for (int j = 0; j < dimension; j++){
-                this.grid[i][j] = (int)(Math.random()*10);
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                this.grid[i][j] = (int) (Math.random() * 10);
             }
         }
     }
 
-    public void showGrid(){
-        for (int i = 0; i < dimension; i++){
-            for (int j = 0; j < dimension; j++){
-                System.out.println(grid[i][j] + " ");
+    public void showGrid() {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    public int[][] get(){
+        return grid;
     }
 }
